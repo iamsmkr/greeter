23f938596f3818233a80e925aac09fc788da4cca
iamsmkr <mailtoshivamk@gmail.com>
2021-07-28 05:39:16 +0000

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "sonatype"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.12.4"
---
+++ src/main/scala/com/shivamkapoor/sonatype/Greeter.scala
@@ -0,0 +1,6 @@
+package com.shivamkapoor.sonatype
+
+object Greeter {
+
+  def greet(name: String): String = s"Hello $name!"
+}
